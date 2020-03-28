a8cc86c0a2f3d01c1b0a554f211fe931bbf70797
Kaushal <kaushal.v.bhat@gmail.com>
2020-03-28 17:44:45 +0530

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,35 +1,14 @@
-//const name = document.getElementById('name').value;
-//const message = document.getElementById('message').value;
-let saveFile = () => {
-    	
-    // Get the data from each element on the form.
-    const name1 = document.getElementById('name');
-    const message1 = document.getElementById('message');
+const slide = document.body;
 
-    console.log(name);
-    
-    // This variable stores all the data.
-    let data = 
-            'let m5 = "' + message1.value + '";';
-        
-    // the \r\n is for new line or line breaks, so that each data can be written in a new line in the text file
+function doThis() {
+  // let name1 = document.getElementById('name'),value;
+  let message1 = document.getElementById("message").value;
 
-    // Convert the text to BLOB.
-    const textToBLOB = new Blob([data], { type: 'text/plain' });
-    const sFileName = 'formData.txt';	   // The file to save the data.
+  window.localStorage.setItem("message_", message1);
 
-    let newLink = document.createElement("a");
-    newLink.download = sFileName;
-    // newLink.append = sFileName;
+  let temp = window.localStorage.getItem("message_");
 
-    if (window.webkitURL != null) {
-        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
-    }
-    else {
-        newLink.href = window.URL.createObjectURL(textToBLOB);
-        newLink.style.display = "none";
-        document.body.appendChild(newLink);
-    }
+  console.log(temp);
 
-    newLink.click(); 
+  document.getElementById("m5").innerHTML = temp;
 }
