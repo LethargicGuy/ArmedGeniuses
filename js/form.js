870da6950e62dbb34eb3d08079fbf4dd4845961a
Kaushal <kaushal.v.bhat@gmail.com>
2020-04-05 16:23:13 +0530

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,14 +1,49 @@
-const slide = document.body;
 
-function doThis() {
-  // let name1 = document.getElementById('name'),value;
-  let message1 = document.getElementById("message").value;
+document.getElementById("m5").innerHTML = localStorage.getItem("messageSlide");
 
-  window.localStorage.setItem("message_", message1);
+function validateMesssage() {
+  let userName2 = document.getElementById("name12").value;
+  //console.log(userName2);
+  let userMessage2 = document.getElementById("message12").value;
+  //console.log(userMessage2);
 
-  let temp = window.localStorage.getItem("message_");
+  if (validateName(userName2, 5, 15)) {
+    if (validateMesssage2(userMessage2, 70)) {
+      alert("Your feeedback has been taken, and added to the slideshow");
+      doThis(userMessage2);
+    }
+  }
+}
 
-  console.log(temp);
+function validateName(uN, lL, uL) {
+  let un_len = uN.length;
+  if (un_len == 0 || un_len >= uL || un_len < lL) {
+    alert(
+      "Name should not be empty, length must be between " + lL + " and " + uL
+    );
+    console.log("Name is not validated.");
+    return false;
+  }
+  console.log("name is validated");
+  return true;
+}
 
-  document.getElementById("m5").innerHTML = temp;
+function validateMesssage2(uM, limit) {
+  let userMLength = uM.length;
+
+  if (userMLength == 0 || userMLength >= limit) {
+    alert("Message should not be empty / length be smaller than " + limit);
+    console.log("Message is not validated.");
+    return false;
+  }
+  console.log("Message is validated");
+  return true;
+}
+
+function doThis(userMessage2) {
+  localStorage.setItem("messageSlide", userMessage2);
+
+  document.getElementById("m5").innerHTML = localStorage.getItem(
+    "messageSlide"
+  );
 }
