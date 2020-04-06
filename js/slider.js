2353c972cb402c6b8753b11a5e114b648e824346
Kaushal <kaushal.v.bhat@gmail.com>
2020-04-06 20:13:56 +0530

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,4 @@
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -12,7 +13,6 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -20,11 +20,8 @@ function showSlides(n) {
     slideIndex = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = "none"; // for the slide to cycle, and not display print a new one
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" dotActive", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " dotActive";
+  
+  slides[slideIndex - 1].style.display = "block"; // to display the slide
 }
